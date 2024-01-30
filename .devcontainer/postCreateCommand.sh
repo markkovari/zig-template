6ed3a7098495a6c85277441f83a686b236cf0925
Márk Kővári <kovarimarkofficial@gmail.com>
2024-01-30 20:25:34 +0100

    chore: create folders

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,10 @@
-pushd .devcontainer
+cd .devcontainer
+mkdir -p zls zig
 
+# Install ZLS
 zls_version="0.11.0"
 zls_tar_name="zls-x86_64-linux.tar.gz"
-# Install ZLS
-pushd zls
+cd zls
 if [ -f $zls_tar_name ]; then
     echo "current version of zls ${zls_version} exists, skipping download"
 else
@@ -11,18 +12,16 @@ else
     tar -x --strip-components=1 -f "$zls_tar_name" --no-same-owner
 fi
 chmod -R 777 ./bin/
-popd
+cd ..
 
 # Install zig
-pushd zig
+cd zig
 zig_version="0.12.0-dev.2341+92211135f"
 zig_tar_name="zig-linux-x86_64-${zig_version}.tar.xz"
-if [ -f $zig_tar_name]; then
+if [ -f $zig_tar_name ]; then
     echo "current version of zig ${zig_version} exists, skipping download"
 else 
     wget "https://ziglang.org/builds/zig-linux-x86_64-${zig_version}.tar.xz"
     tar -x --strip-components=1 -f $zig_tar_name --no-same-owner
 fi
 chmod -R 777 ./zig
-popd
-popd
